483c83a2b32cf2b7c9ee37ffc66f9a8846e72e9b
Gary Giambatista <gary.giambatista@gmail.com>
2022-11-14 19:51:33 -0500

    changed config files for uploading to github pages

---
+++ gary.giambatista.com/next.config.mjs
@@ -0,0 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+	images: {
+		loader: "akamai",
+		path: "",
+	},
+	assetPrefix: "./",
+};
